b30ca41f07134d7545b0863fbbc6fd62526f7b2b
yoluquitas <lksleiva@gmail.com>
2016-08-08 02:03:56 -0300

    Upgrade GraphView lib de 3.0 a 4.1.1 Solucion Issue refresco de pantalla  en Android API19
    Se modifico la bastante el codigo para actualizar la libreria Graph View
    de la version 3.0 a la 4.1.1
    El programa queda en esta instancia con varios issues a resolver y a
    testear.  Pero se resolvio el problema que se presentaba en la version
    anterior con la libreria 3.0 que no actualizaba la pantalla en versiones
    de Android con API 19.
    Por el momento se dejo la funcion AppendData en un thread, pero no esta
    bien hecho hay que mejorarlo.
    Se puede grabar y elegir carpeta y nombre de archivo.
    Se debe agregar la posibilidad de sobrescribir un archivo, ya que
    actualmente no funciona esa opcion.

--- app/build/generated/source/r/debug/com/example/tut1/R.java
+++ app/build/generated/source/r/debug/com/example/tut1/R.java
@@ -9,6 +9,39 @@ package com.example.tut1;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesColor=0x7f010003;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesData=0x7f010000;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesTitle=0x7f010002;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesType=0x7f010001;
     }
     public static final class color {
         public static final int black=0x7f050000;
@@ -24,8 +57,6 @@ public final class R {
     }
     public static final class id {
         public static final int Graph=0x7f080004;
-        public static final int Graph_0=0x7f080005;
-        public static final int Graph_1=0x7f080006;
         public static final int LL2=0x7f080007;
         public static final int LLX=0x7f080010;
         public static final int bConnect=0x7f080009;
@@ -34,6 +65,8 @@ public final class R {
         public static final int bXminus=0x7f080011;
         public static final int bXplus=0x7f080012;
         public static final int bg=0x7f080003;
+        public static final int graphView_0=0x7f080005;
+        public static final int graphView_1=0x7f080006;
         public static final int listView=0x7f080001;
         public static final int menu_settings=0x7f080013;
         public static final int tbLock=0x7f08000f;
@@ -60,4 +93,91 @@ public final class R {
     public static final class style {
         public static final int AppTheme=0x7f040000;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a GraphViewXML.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #GraphViewXML_android_title android:title}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesColor com.example.tut1:seriesColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesData com.example.tut1:seriesData}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesTitle com.example.tut1:seriesTitle}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesType com.example.tut1:seriesType}</code></td><td></td></tr>
+           </table>
+           @see #GraphViewXML_android_title
+           @see #GraphViewXML_seriesColor
+           @see #GraphViewXML_seriesData
+           @see #GraphViewXML_seriesTitle
+           @see #GraphViewXML_seriesType
+         */
+        public static final int[] GraphViewXML = {
+            0x010101e1, 0x7f010000, 0x7f010001, 0x7f010002,
+            0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link android.R.attr#title}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+          @attr name android:title
+        */
+        public static final int GraphViewXML_android_title = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.example.tut1.R.attr#seriesColor}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.tut1:seriesColor
+        */
+        public static final int GraphViewXML_seriesColor = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.example.tut1.R.attr#seriesData}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.tut1:seriesData
+        */
+        public static final int GraphViewXML_seriesData = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.example.tut1.R.attr#seriesTitle}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.tut1:seriesTitle
+        */
+        public static final int GraphViewXML_seriesTitle = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.example.tut1.R.attr#seriesType}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.tut1:seriesType
+        */
+        public static final int GraphViewXML_seriesType = 2;
+    };
 }
--- app/src/main/java/com/example/tut1/MainActivity.java
+++ app/src/main/java/com/example/tut1/MainActivity.java
@@ -12,20 +12,18 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-import android.view.View.OnClickListener;
 
 import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.GraphView.GraphViewData;
-import com.jjoe64.graphview.GraphView.LegendAlign;
-import com.jjoe64.graphview.GraphViewSeries;
-import com.jjoe64.graphview.GraphViewSeries.GraphViewStyle;
-import com.jjoe64.graphview.LineGraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -33,18 +31,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
-//TODO: [LL]: Obtener el canal implicito en el paquete y graficar segun corresponda
+public class MainActivity extends Activity implements View.OnClickListener {
+	double Data_Graph_0 = 0, Data_Graph_1 = 0;
 
-public class MainActivity extends Activity implements View.OnClickListener{
-
-    public static final int MIN_Y_Grap_0 = -8000000, MIN_Y_Grap_1 = -8000000, MAX_Y_Grap_0 = 8000000, MAX_Y_Grap_1 = 8000000;
+	public static final int MIN_Y_Grap_0 = -8000000, MIN_Y_Grap_1 = -8000000, MAX_Y_Grap_0 = 8000000, MAX_Y_Grap_1 = 8000000;
 	SimpleFileDialog FileSaveDialog;
 
 	@Override
 	public void onBackPressed() {
 		// TODO Auto-generated method stub
 		if (Bluetooth.connectedThread != null) {
-			Bluetooth.connectedThread.write("Q");}//Stop streaming
+			Bluetooth.connectedThread.write("Q");
+		}//Stop streaming
 		super.onBackPressed();
 	}
 
@@ -61,13 +59,107 @@ public class MainActivity extends Activity implements View.OnClickListener{
 	ToggleButton tbStream;
 	ToggleButton tbRecord;
 	//GraphView init
-	static LinearLayout GraphView_0, GraphView_1;
+	//static LinearLayout GraphView_0, GraphView_1;
 	static GraphView graphView_0, graphView_1;
-	static GraphViewSeries Series_0, Series_1;
+	//static GraphViewSeries Series_0, Series_1;
+	LineGraphSeries<DataPoint> Series_0, Series_1;
 	//graph value
-	private static double graph2LastXValue_0 =0, graph2LastXValue_1 =0;
-	private static int Xview=10;
+	private static double graph2LastXValue_0 = 0, graph2LastXValue_1 = 0;
+	private static double Xview = 20; //60
+	private static boolean DataToGraph= false;
 	Button bConnect, bDisconnect;
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// we're going to simulate real time with thread that append data to the graph
+
+			new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					if(DataToGraph) {
+						DataToGraph= false;
+						// add 1000 new entries
+						for (int i = 0; i < 1000; i++) {
+							if (i == 9) {
+								i = 0;
+							}
+							runOnUiThread(new Runnable() {
+
+								@Override
+								public void run() {
+									addEntry();
+								}
+							});
+
+							// sleep to slow down the add of entries
+							try {
+								Thread.sleep(300);
+
+							} catch (InterruptedException e) {
+								// manage error ...
+								Toast.makeText(getApplicationContext(), "Error en Thread!", 0).show();
+							}
+						}
+					}
+					onResume();
+				}
+			}).start();
+		}
+
+	// add random data to graph
+	private void addEntry() {
+		// here, we choose to display max 40 points on the viewport and we scroll to end
+		Series_0.appendData(new DataPoint(graph2LastXValue_0, Data_Graph_0), AutoScrollX, 1000);
+		Series_1.appendData(new DataPoint(graph2LastXValue_1, Data_Graph_1), AutoScrollX, 1000);
+	}
+
+
+/*
+		private final Handler mHandler_Graph = new Handler();
+		private Runnable mTimer1;
+		private Runnable mTimer2;
+
+		@Override
+		public void onAttach(Activity activity) {
+			super.onAttach(activity);
+			((MainActivity) activity).onSectionAttached(
+					getArguments().getInt(MainActivity.ARG_SECTION_NUMBER));
+		}
+
+		@Override
+		public void onResume() {
+			super.onResume();
+
+			mTimer1 = new Runnable() {
+				@Override
+				public void run() {
+					graph2LastXValue_0 += 1d;
+					Series_0.appendData(new DataPoint(graph2LastXValue_0, Data_Graph_0), AutoScrollX, 40);
+					mHandler_Graph.postDelayed(this, 300);
+				}
+			};
+			mHandler_Graph.postDelayed(mTimer1, 300);
+
+			mTimer2 = new Runnable() {
+				@Override
+				public void run() {
+					graph2LastXValue_1 += 1d;
+					Series_1.appendData(new DataPoint(graph2LastXValue_1, Data_Graph_1), AutoScrollX, 40);
+					mHandler_Graph.postDelayed(this, 200);
+				}
+			};
+			mHandler_Graph.postDelayed(mTimer2, 1000);
+		}
+
+		@Override
+		public void onPause() {
+			mHandler_Graph.removeCallbacks(mTimer1);
+			mHandler_Graph.removeCallbacks(mTimer2);
+			super.onPause();
+		}
+	*/
 
 	private Handler mHandler = new Handler() {
 		@Override
@@ -86,13 +178,14 @@ public class MainActivity extends Activity implements View.OnClickListener{
 					for (int i = 0; i < (readBuf.length -4) ; i++) {  //[LL]: Barro el buffer de 1024 bytes
 						int data_aux[] = new int[]{(int) readBuf[i]&0xff, (int) readBuf[i + 1]&0xff, (int) readBuf[i + 2]&0xff, (int) readBuf[i + 3]&0xff};
 						if ((data_aux[0] & 0x80) == 0x80 && (data_aux[1] & 0x80) == 0 && (data_aux[2] & 0x80) == 0 && (data_aux[3] & 0x80) == 0) {
-							String DataWrite = null;;
-							try {
-								DataWrite = new String(readBuf, i, 4, "ISO-8859-1"); //"ISO-8859-1" encoding para que tome correctamente los extended ascii
-							} catch (UnsupportedEncodingException e) {
-								e.printStackTrace();
-							}
+
 							if(Record==true) {
+								String DataWrite = null;;
+								try {
+									DataWrite = new String(readBuf, i, 4, "ISO-8859-1"); //"ISO-8859-1" encoding para que tome correctamente los extended ascii
+								} catch (UnsupportedEncodingException e) {
+									e.printStackTrace();
+								}
 								String DirToSaveType;
 								if (FileSaveDialog.Get_m_dir().equals("") ==false)
 									DirToSaveType= FileSaveDialog.Get_m_dir();
@@ -104,43 +197,50 @@ public class MainActivity extends Activity implements View.OnClickListener{
 							i = i + 3;
 
 							//SaveData(Double.toString(data) ,"Datos.txt"); //Guardo los datos en un archivo
-
+							DataToGraph=true;
 							switch(channel)
 							{
 								case 0:
-									Series_0.appendData(new GraphViewData(graph2LastXValue_0, data), AutoScrollX);
+									Data_Graph_0 = data;
+
+									//Series_0.appendData(new DataPoint(graph2LastXValue_0, data), AutoScrollX, 40);
 									//X-axis control
 									if (graph2LastXValue_0 >= Xview && Lock == true) {
-										Series_0.resetData(new GraphViewData[]{});
+										Series_0.resetData(new DataPoint[]{});
 										graph2LastXValue_0 = 0;
 									} else graph2LastXValue_0 += 0.1;
 
 									if (Lock == true) {
-										graphView_0.setViewPort(0, Xview);
+										graphView_0.getViewport().setMinX(0);
+										graphView_0.getViewport().setMaxX(Xview);
 									} else {
-										graphView_0.setViewPort(graph2LastXValue_0 - Xview, Xview);
+										graphView_0.getViewport().setMinX(graph2LastXValue_0);
+										graphView_0.getViewport().setMaxX(Xview + graph2LastXValue_0);
 									}
-									//refresh
-									GraphView_0.removeView(graphView_0);
-									GraphView_0.addView(graphView_0);
+
 									break;
 
 								case 1:
-									Series_1.appendData(new GraphViewData(graph2LastXValue_1, data), AutoScrollX);
+									Data_Graph_1 = data;
+
+									//Series_1.appendData(new DataPoint(graph2LastXValue_1, data), AutoScrollX, 40);
+
 									//X-axis control
 									if (graph2LastXValue_1 >= Xview && Lock == true) {
-										Series_1.resetData(new GraphViewData[]{});
+										Series_1.resetData(new DataPoint[]{});
 										graph2LastXValue_1 = 0;
-									} else graph2LastXValue_1 += 0.1;
+									} else
+										graph2LastXValue_1 += 0.1;
 
 									if (Lock == true) {
-										graphView_1.setViewPort(0, Xview);
+										graphView_1.getViewport().setMinX(0);
+										graphView_1.getViewport().setMaxX(Xview);
 									} else {
-										graphView_1.setViewPort(graph2LastXValue_1 - Xview, Xview);
+										graphView_1.getViewport().setMinX(graph2LastXValue_1);
+										graphView_1.getViewport().setMaxX(Xview + graph2LastXValue_1);
 									}
-									//refresh
-									GraphView_1.removeView(graphView_1);
-									GraphView_1.addView(graphView_1);
+
+
 									break;
 							}
 						}
@@ -181,47 +281,64 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		Bluetooth.gethandler(mHandler);
 
 		//init graphview
-		GraphView_0 = (LinearLayout) findViewById(R.id.Graph_0);
-		// init example series data------------------- 
-		Series_0 = new GraphViewSeries("Signal",
-				new GraphViewStyle(Color.YELLOW, 2),//color and thickness of the line 
-				new GraphViewData[] {new GraphViewData(0, 0)});
-		graphView_0 = new LineGraphView(
-				this // context  
-				, "Graph" // heading  
-				);
-		graphView_0.setViewPort(0, Xview);
-		graphView_0.setScrollable(true);
-		graphView_0.setScalable(true);
-		graphView_0.setShowLegend(true);
-		graphView_0.setLegendAlign(LegendAlign.BOTTOM);
-		graphView_0.setManualYAxis(true);
-		//graphView_0.setManualYAxisBounds(10000, 0);
-		graphView_0.setManualYAxisBounds(MAX_Y_Grap_0, MIN_Y_Grap_0);
-		graphView_0.addSeries(Series_0); // data
-		GraphView_0.addView(graphView_0);
+		graphView_0 = (GraphView) findViewById(R.id.graphView_0);
+		Series_0 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+				new DataPoint(0, 0),
+		});
+		// styling series
+		Series_0.setTitle("EEG");
+		Series_0.setColor(Color.YELLOW);
+		//Series_0.setDrawDataPoints(true);
+		//Series_0.setDataPointsRadius(10);
+		Series_0.setThickness(4);
+		graphView_0.getLegendRenderer().setVisible(true);
+		graphView_0.getViewport().setScrollable(true);      // enable scrolling
+		graphView_0.getViewport().setScalable(true);        // enable scaling
+		graphView_0.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+		graphView_0.getGridLabelRenderer().setHorizontalLabelsVisible(true);
+		graphView_0.getGridLabelRenderer().setVerticalLabelsVisible(true);
+		graphView_0.getViewport().setXAxisBoundsManual(true);
+		graphView_0.getViewport().setMinX(0);
+		graphView_0.getViewport().setMaxX(Xview);
+		graphView_0.addSeries(Series_0);
 
-
-		//[LL]
-		//init graphview
-		GraphView_1 = (LinearLayout) findViewById(R.id.Graph_1);
 		// init example series data-------------------
-		Series_1 = new GraphViewSeries("Signa2",
-				new GraphViewStyle(Color.RED, 2),//color and thickness of the line
-				new GraphViewData[] {new GraphViewData(0, 0)});
-		graphView_1 = new LineGraphView(
+
+		/*
+		Series_0 = new GraphViewSeries("Signal",
+				new GraphViewStyle(Color.YELLOW, 2),//color and thickness of the line
+				new DataPoint[] {new DataPoint(0, 0)});
+		graphView_0 = new LineGraphView(
 				this // context
 				, "Graph" // heading
-		);
-		graphView_1.setViewPort(0, Xview);
-		graphView_1.setScrollable(true);
-		graphView_1.setScalable(true);
-		graphView_1.setShowLegend(true);
-		graphView_1.setLegendAlign(LegendAlign.BOTTOM);
-		graphView_1.setManualYAxis(true);
-		graphView_1.setManualYAxisBounds(MAX_Y_Grap_1, MIN_Y_Grap_1);
-		graphView_1.addSeries(Series_1); // data
-		GraphView_1.addView(graphView_1);
+				);
+		graphView_0.setViewPort(0, Xview);
+		//graphView_0.setManualYAxis(true);
+		//graphView_0.setManualYAxisBounds(10000, 0);
+		//graphView_0.setManualYAxisBounds(MAX_Y_Grap_0, MIN_Y_Grap_0);
+*/
+
+		graphView_1 = (GraphView) findViewById(R.id.graphView_1);
+		Series_1 = new LineGraphSeries<DataPoint>(new DataPoint[]{
+				new DataPoint(0, 0),
+		});
+		// styling series
+		Series_1.setTitle("EOG");
+		Series_1.setColor(Color.RED);
+		//Series_1.setDrawDataPoints(true);
+		//Series_1.setDataPointsRadius(10);
+		Series_1.setThickness(4);
+		graphView_1.getViewport().setScrollable(true);      // enable scrolling
+		graphView_1.getViewport().setScalable(true);        // enable scaling
+		graphView_1.getLegendRenderer().setVisible(true);
+		graphView_1.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.BOTTOM);
+		graphView_1.getGridLabelRenderer().setHorizontalLabelsVisible(true);
+		graphView_1.getGridLabelRenderer().setVerticalLabelsVisible(true);
+		graphView_1.getViewport().setXAxisBoundsManual(true);
+		graphView_1.getViewport().setMinX(0);
+		graphView_1.getViewport().setMaxX(Xview);
+		graphView_1.addSeries(Series_1);
+
 	}
 
 	void ButtonInit(){
@@ -249,7 +366,6 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		public void onClick(View v) {
 
 			if (tbRecord.isChecked()){
-				Record = true;
 				/////////////////////////////////////////////////////////////////////////////////////////////////
 				//Create FileOpenDialog and register a callback
 				/////////////////////////////////////////////////////////////////////////////////////////////////
@@ -271,6 +387,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
 				FileSaveDialog.chooseFile_or_Dir();
 
 				/////////////////////////////////////////////////////////////////////////////////////////////////
+				Record = true;
 			} else{
 				Record = false;
 			}
@@ -299,7 +416,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
 			if (Xview>1) Xview--;
 			break;
 		case R.id.bXplus:
-			if (Xview<30) Xview++;
+			//if (Xview<30) Xview++;
+			if (Xview<100) Xview++;
 			break;
 		case R.id.tbLock:
 			if (tbLock.isChecked()){
@@ -318,7 +436,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		case R.id.tbStream:
 			if (tbStream.isChecked()){
 				if (Bluetooth.connectedThread != null)
-					Bluetooth.connectedThread.write("E");
+					Bluetooth.connectedThread.write("5");
+					//Bluetooth.connectedThread.write(Character.toString((char)5));
 			}else{
 				if (Bluetooth.connectedThread != null)
 					Bluetooth.connectedThread.write("Q");
